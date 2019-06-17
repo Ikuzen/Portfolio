0f7ce096fbac5c0a262c5cd0021d1fc4922b12ee
Yannis <yannis.samba@gmail.com>
2019-06-17 12:38:08 +0200

    add animation 3D

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,18 @@
+
+
+(function(){
 let start = document.getElementById("start")
 let header = document.getElementsByTagName("header")
 let content = document.getElementById("content")
-console.log(header[0])
+let canvas = document.getElementsByTagName("canvas")
 function reveal() {
     content.classList.remove("future")
     content.classList.add("present") 
     header[0].classList.remove("present")
     header[0].classList.add("past")
+    canvas[0].classList.add("past")
+    cancelAnimationFrame(animate) /// cancels the animation ///
 }
 start.addEventListener("click", reveal)
+
+})();
